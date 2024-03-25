51b68ff1c796acabf663213d2f2aad0da3e85756
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2024-03-25 15:35:15 -0300

    Bugfix: Cálculo de velocidade média deve ser feito com base no nº de imagens

--- src/services/inference.ts
+++ src/services/inference.ts
@@ -11,8 +11,15 @@ const inferenceServices = {
 
     getMediumSpeed: async (selectionArgs:  Prisma.InferenceWhereInput | undefined) => {
         const inferences = await prisma.inference.findMany({where: selectionArgs});
-        if(inferences.length == 0) return null
-        return inferences.map(inf => inf.inf_speed).reduce((acc, curr) => acc + curr) / inferences.length;
+        if(inferences.length === 0) return null
+        let totalImages = 0
+        let totalSpeed = 0
+        for(let inf of inferences) {
+            totalSpeed += inf.inf_speed * inf.num_images
+            totalImages += inf.num_images
+        }
+        if(totalImages === 0) return null
+        return totalSpeed / totalImages 
     },
 
     getInference: (id: number) => 
