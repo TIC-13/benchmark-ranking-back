2e032f48959767b7138229f470395f7ea8ca3200
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2024-03-25 16:50:14 -0300

    Cors liberado

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import express from 'express'
 import phoneRouter from './routes/phone';
 import inferenceRouter from './routes/inference';
 import errorHandler from './middleware/error';
+import cors from 'cors';
 
 const app = express()
+
+app.use(cors());
 
 app.use(express.json());
 
