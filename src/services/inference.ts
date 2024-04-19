649213ca30c0d8cc12bef67560135d60f803b28c
ThalesBezerra21 <thalesoliveira.bezerra@gmail.com>
2024-04-19 09:45:44 -0300

    Valor da medição da  GPU e medições em ms podem ser null

--- src/services/inference.ts
+++ src/services/inference.ts
@@ -11,11 +11,12 @@ const inferenceServices = {
 
     getMediumSpeed: async (selectionArgs:  Prisma.InferenceWhereInput | undefined) => {
         const inferences = await prisma.inference.findMany({where: selectionArgs});
+        const inferencesNotNull = inferences.filter(x => x.inf_speed !== null)
         if(inferences.length === 0) return null
         let totalImages = 0
         let totalSpeed = 0
-        for(let inf of inferences) {
-            totalSpeed += inf.inf_speed * inf.num_images
+        for(let inf of inferencesNotNull) {
+            totalSpeed += inf.inf_speed! * inf.num_images
             totalImages += inf.num_images
         }
         if(totalImages === 0) return null
